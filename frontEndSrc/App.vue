07b394c30b1ceb05291b6646bbf49d0d3c44e388
Alexey Ulyanov <lexxsol@yandex.ru>
2021-08-17 22:02:13 +0400

    add empty Registers page

--- frontEndSrc/App.vue
+++ frontEndSrc/App.vue
@@ -6,6 +6,9 @@
 			</router-link> |
 			<router-link to="/about">
 				About
+			</router-link> |
+			<router-link to="/registers">
+				Registers
 			</router-link>
 		</div>
 		<router-view />
--- frontEndSrc/router/index.js
+++ frontEndSrc/router/index.js
@@ -16,6 +16,15 @@ const routes = [
        which is lazy-loaded when the route is visited. */
 		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
 	},
+	{
+		path: '/registers',
+		name: 'Registers',
+
+		/* route level code-splitting
+       this generates a separate chunk (about.[hash].js) for this route
+       which is lazy-loaded when the route is visited. */
+		component: () => import(/* webpackChunkName: "registers" */ '../views/Registers.vue'),
+	},
 ];
 
 const router = createRouter({
